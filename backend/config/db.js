fa39eb765f56202301cdacffe09752e365282d47
Pravin  Rathod <143316584+PravinRathod009@users.noreply.github.com>
2026-08-20 18:01:15 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://heshrox:rgboijhgasfgh@cluster0.tw9dm91.mongodb.net/campmart?retryWrites=true&w=majority');
+    const conn = await mongoose.connect('mongodb+srv://heshrox:rgboijhgasfgh@cluster0.tw9dm91.mongodb.net/?appName=Cluster0');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
